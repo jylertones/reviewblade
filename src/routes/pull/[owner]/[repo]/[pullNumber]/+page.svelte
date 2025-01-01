fab4e4cbfe90221ba1dd0498e111689e1a30c312
Tyler Jones <tyler@doublejones.com>
2025-01-01 17:16:42 -0500

    Improve code in PR description

--- src/routes/pull/[owner]/[repo]/[pullNumber]/+page.svelte
+++ src/routes/pull/[owner]/[repo]/[pullNumber]/+page.svelte
@@ -64,7 +64,9 @@
 	<h2>Description</h2>
 
 	<!-- eslint-disable-next-line svelte/no-at-html-tags -->
-	{@html data.body}
+	<div class="description-body">
+		{@html data.body}
+	</div>
 </section>
 
 <section>
@@ -107,6 +109,28 @@
 		.contains-task-list {
 			list-style: none;
 			padding-inline-start: 1rem;
+		}
+	}
+
+	.description-body :global {
+		code {
+			background-color: var(--background-color-tertiary);
+			font-size: var(--font-body-size-2);
+			padding-inline: 0.25rem;
+			padding-block: 0.25rem;
+			border-radius: var(--border-radius);
+		}
+
+		pre {
+			background-color: var(--background-color-tertiary);
+			padding: 0.5rem;
+			border-radius: var(--border-radius);
+		}
+
+		pre > code {
+			background-color: transparent;
+			padding: 0;
+			border-radius: 0;
 		}
 	}
 </style>
