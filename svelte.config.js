85148eb11043ebf81a9004cf2c14f9e9911ee1a8
Tyler Jones <tyler@doublejones.com>
2025-01-06 21:26:09 -0500

    Cloudflare instead of DigitalOcean static

--- svelte.config.js
+++ svelte.config.js
@@ -12,13 +12,17 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: '200.html',
-			precompress: false,
-			strict: true,
+			// See below for an explanation of these options
+			routes: {
+				include: ['/*'],
+				exclude: ['<all>'],
+			},
+			platformProxy: {
+				configPath: 'wrangler.toml',
+				environment: undefined,
+				experimentalJsonConfig: false,
+				persist: false,
+			},
 		}),
 	},
 };
