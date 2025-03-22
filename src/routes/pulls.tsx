81049292ce5378b912d28449d5713c47ad5295a3
Tyler Jones <tyler@doublejones.com>
2025-03-21 21:48:30 -0400

    Better loading for pulls, no flashing

--- src/routes/pulls.tsx
+++ src/routes/pulls.tsx
@@ -1,8 +1,9 @@
 import { MetaProvider, Title } from "@solidjs/meta";
-import { Show } from "solid-js";
+import { Show, Suspense } from "solid-js";
 import { getSearchPullRequests } from "~/api/getSearchPullRequests";
 import { Flex } from "~/components/Flex/Flex";
 import { PullRequestList } from "~/components/PullRequestList/PullRequestList";
+import { Text } from "~/components/Text/Text";
 
 export default function PullsPage() {
   const myPullRequestsQuery = getSearchPullRequests({
@@ -18,23 +19,23 @@ export default function PullsPage() {
       <Title>Pull requests</Title>
       <h1>Pull requests</h1>
 
-      <Show when={!myPullRequestsQuery.isLoading}>
+      <Suspense fallback={<Text>Loading pull requests</Text>}>
         <PullRequestList
           title={`My pull requests (${myPullRequestsQuery.data?.data.total_count})`}
           pullRequests={myPullRequestsQuery.data?.data.items ?? []}
           isError={myPullRequestsQuery.isError}
           noRequestsMessage="👌 You don't have any outstanding pull requests"
         />
-      </Show>
+      </Suspense>
 
-      <Show when={!myReviewPullRequestsQuery.isLoading}>
+      <Suspense fallback={<Text>Loading code reviews</Text>}>
         <PullRequestList
           title={`My reviews (${myReviewPullRequestsQuery.data?.data.total_count})`}
           pullRequests={myReviewPullRequestsQuery.data?.data.items ?? []}
           isError={myReviewPullRequestsQuery.isError}
           noRequestsMessage="✅ You're all caught up with code reviews!"
         />
-      </Show>
+      </Suspense>
     </Flex>
   );
 }
